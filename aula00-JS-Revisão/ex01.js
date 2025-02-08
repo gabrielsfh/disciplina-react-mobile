0a781757d1ae4015a15abfd48e5cfa6fdbf9af0a
gabriel <90591801+gabrielsfh@users.noreply.github.com>
2025-02-08 00:26:07 +0000

    diferença let e var

---
+++ aula00-JS-Revisão/ex01.js
@@ -0,0 +1,26 @@
+// Exemplo 1: Variaáveis (var, let e const)
+
+// Usando var(escopo global/função)
+
+let nome = "Biel";
+
+if(true){
+    let nome = "Maria";
+    console.log(nome);
+}
+
+console.log(nome);
+
+
+
+
+/*
+var nome = "Biel";
+
+if(true){
+    var nome = "Maria";
+    console.log(nome);
+}
+
+console.log(nome);
+*/
