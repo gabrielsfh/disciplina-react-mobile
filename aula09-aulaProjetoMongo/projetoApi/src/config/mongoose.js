6f312eec024b3fbddaf0b90fd62aedd7094d5cf1
gabriel <90591801+gabrielsfh@users.noreply.github.com>
2025-05-10 00:30:36 +0000

    aula mongo + api 2

--- aula09-aulaProjetoMongo/projetoApi/src/config/mongoose.js
+++ aula09-aulaProjetoMongo/projetoApi/src/config/mongoose.js
@@ -5,16 +5,14 @@ dotenv.config();
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/meubanco';
 
 export async function connectDB() {
-    try {
-        await mongoose.connect(
-            MONGO_URI,
-            {
-                useNewUrlParser: true,
-                userUnifiedTopology: true
-            });
-        console.log('MongoDB conectado em', MONGO_URI);
-    } catch (err) {
-        console.error('Erro ao coenctar MongoDB:', err);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB conectado em', MONGO_URI);
+  } catch (err) {
+    console.error('Erro ao conectar MongoDB:', err);
+    process.exit(1);
+  }
 }
--- aula09-aulaProjetoMongo/projetoApi/src/services/produtoService.js
+++ aula09-aulaProjetoMongo/projetoApi/src/services/produtoService.js
@@ -1,22 +1,22 @@
-import { produtoRepository } from "../repositories/produtoRepository";
+import { produtoRepository } from '../repositories/produtoRepository.js';
 
 export const produtoService = {
-    async criarProduto(dto) {
-        if(dto.preco < 0) throw new Error('Preço negativo não permitido');
-        return produtoRepository.cria(dto);
-    },
-    async listarProdutos(){
-        return produtoRepository.listarTodos();
-    },
-    async obterProduto(id){
-        const p = await produtoRepository.buscarPorID(id);
-        if(!p) throw new Error('Produto não encontrado');
-        return p;
-    },
-    async atualizarProduto(id, dto){
-        return produtoRepository.atualizar(id, dto);
-    },
-    async removeProduto(id){
-        return produtoRepository.delete(id);
-    }
-}
+  async criarProduto(dto) {
+    if (dto.preco < 0) throw new Error('Preço negativo não permitido');
+    return produtoRepository.criar(dto);
+  },
+  async listarProdutos() {
+    return produtoRepository.listarTodos();
+  },
+  async obterProduto(id) {
+    const p = await produtoRepository.buscarPorId(id);
+    if (!p) throw new Error('Produto não encontrado');
+    return p;
+  },
+  async atualizarProduto(id, dto) {
+    return produtoRepository.atualizar(id, dto);
+  },
+  async removerProduto(id) {
+    return produtoRepository.deletar(id);
+  }
+};
